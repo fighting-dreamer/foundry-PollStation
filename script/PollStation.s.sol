407e63a02fbde11ba9940a5049f8fd14f301e9d8
Nipun Jindal <mansapatiala@gmail.com>
2025-10-08 18:53:56 +0530

    todo: deployment on arbitrum sepolia and verifying, doing same for other chains

--- script/PollStation.s.sol
+++ script/PollStation.s.sol
@@ -10,10 +10,17 @@ contract PollStationScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint privateKey = vm.envUint("WALLET_PRIVATE_KEY");
+        address addr = vm.addr(privateKey);
+        console.log("address", addr);
+        console.log("Chain ID:", block.chainid);
+        vm.startBroadcast(privateKey);
 
-        // ps = new PollStation();
+        address[] memory candidates = new address[](2);
+        candidates[0] = 0x90F79bf6EB2c4f870365E785982E1f101E93b906; // test address 3 in anvil
+        candidates[1] = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65; // test address 4 in anvil
 
+        ps = new PollStation(candidates);
         vm.stopBroadcast();
     }
 }
